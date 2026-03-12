9f6832c72a96c40f13f728ea49aef9ce339f8962
Z3RG <sollwarmorgrind@gmail.com>
2026-03-12 19:54:36 +0300

    ВК Реклама

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,19 @@ export default function RootLayout({
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-T55HC6Q4');`}
         </Script>
+        <Script id="top-mail-ru" strategy="afterInteractive">
+          {`
+            var _tmr = window._tmr || (window._tmr = []);
+            _tmr.push({id: "3745711", type: "pageView", start: (new Date()).getTime()});
+            (function (d, w, id) {
+              if (d.getElementById(id)) return;
+              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+              ts.src = "https://top-fwz1.mail.ru/js/code.js";
+              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+            })(document, window, "tmr-code");
+          `}
+        </Script>
         <noscript>
           <div>
             <img
@@ -101,6 +114,15 @@ export default function RootLayout({
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
+        </noscript>
+        <noscript>
+          <div>
+            <img
+              src="https://top-fwz1.mail.ru/counter?id=3745711;js=na"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt="Top.Mail.Ru"
+            />
+          </div>
         </noscript>
       </body>
     </html>
